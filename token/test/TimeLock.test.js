092da9bf5679ca745c344da106eb5468ebbbfeb3
Ryan Farley <daniel.r.farley@gmail.com>
2021-06-14 16:01:53 -0400

    Audit: Removed factory tests & adjusted for non-factory deployment.

--- token/test/TimeLock.test.js
+++ token/test/TimeLock.test.js
@@ -1,6 +1,5 @@
 const BN = require('bignumber.js');
 const ArtmToken = artifacts.require("ArtmToken");
-const ArtemisTimeLockFactory = artifacts.require('ArtemisTimeLockFactory');
 const ArtemisTimeLock = artifacts.require('ArtemisTimeLock');
 
 const null_address = '0x0000000000000000000000000000000000000000';
@@ -13,6 +12,8 @@ const duration = {
     weeks: function (val) { return val * this.days(7); },
     years: function (val) { return val * this.days(365); },
 }
+
+const timeNow = Math.floor(Date.now()/1000);
 
 async function timeJump(timeToInc) {
   return new Promise((resolve, reject) => {
@@ -31,116 +32,45 @@ async function timeJump(timeToInc) {
   })
 }
 
-contract('ArtemisTimeLockFactory', accounts => {
+contract('ArtemisTimeLock', accounts => {
     beforeEach(async function(){
-        this.contractFactory = await ArtemisTimeLockFactory.deployed();
         this.token = await ArtmToken.deployed();
+        this.timeLock = await ArtemisTimeLock.deployed();
     });
 
-    // Make sure the contract factory was deployed
-    describe("Is deployed", async function(){
-        it("has an address", async function (){
-            //this.contractFactory.address;
-            console.log("Factory Address: " + this.contractFactory.address);
-            this.contractFactory.address.should.not.equal(null_address);
-        })
-
-        // Make sure the contract factory deployed 2 child contracts
-        it("has 1st child contract", async function (){
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            console.log("Address 1: " + contract1_address);
-            contract1_address.should.not.equal(null_address);
-        })
-
-        it("has 2nd child contract", async function (){
-            const contract2_address = await this.contractFactory.timeLocks(1);
-            console.log("Address 2: " + contract2_address);
-            contract2_address.should.not.equal(null_address);
-        })
-
-        it("fails to deploy 3rd contract with null address", async function (){
-            const timeNow = Math.floor(Date.now()/1000);
-            const vestingStart = timeNow + duration.days(1);
-            const unlockTime = timeNow + duration.weeks(24);
-
-            try {
-                await this.contractFactory.createTimeLock(2, this.token.address, null_address, unlockTime, vestingStart, 12, 8, 1250); // using null address
-                assert.fail("Contract deployment should have failed.");
-            } catch (err) {
-                assert.include(err.message, "revert", "The error message should contain 'revert'");
-            }
-
-        })
-
-        it("fails to deploy 3rd contract with invalid percentages", async function (){
-            const timeNow = Math.floor(Date.now()/1000);
-            const vestingStart = timeNow + duration.days(1);
-            const unlockTime = timeNow + duration.weeks(24);
-
-            try {
-                await this.contractFactory.createTimeLock(2, this.token.address, accounts[3], unlockTime, vestingStart, 12, 8, 2500); // requires 100%, provided 200%
-                assert.fail("Contract deployment should have failed.");
-            } catch (err) {
-                assert.include(err.message, "revert", "The error message should contain 'revert'");
-            }
-        })
-
-        it("successfully deploys 3rd contract with correct params", async function (){
-            const timeNow = Math.floor(Date.now()/1000);
-            const vestingStart = timeNow + duration.days(1);
-            const unlockTime = timeNow + duration.weeks(24);
-
-            await this.contractFactory.createTimeLock(2, this.token.address, accounts[3], unlockTime, vestingStart, 12, 8, 1250);
-
-            const contract3_address = await this.contractFactory.timeLocks(2);
-            console.log("Address 2: " + contract3_address);
-            contract3_address.should.not.equal(null_address);
-        })
-
-    })
-    // Make sure the child contract is in good shape
-    describe("Child contract 1 has properties..", async function(){
+    describe("has correct properties..", async function(){
         it("...has correct beneficiary", async function (){
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
-            contract1_beneficiary = await contract1.beneficiary();
+            contract1_beneficiary = await this.timeLock.beneficiary();
             console.log("Contract1 Beneficiary: " + contract1_beneficiary);
             contract1_beneficiary.should.equal(accounts[3]);
         })
 
         it("...has the correct this.token.", async function (){
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-            token_address = await contract1.token();
+            token_address = await this.timeLock.token();
             console.log("Token: " + token_address);
             token_address.should.equal(this.token.address);
         })
 
 
         it("...has a release time.", async function (){
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-            release_time = await contract1.releaseTime();
+            release_time = await this.timeLock.releaseTime();
             release_time.should.not.equal('');
         })
 
         it("...has correct initial token balance", async function (){
-            const contract1_address = await this.contractFactory.timeLocks(0);
+            const contract1_address = await this.timeLock.address;
             const contract_balance = new BN(await this.token.balanceOf(contract1_address)).div(new BN(10).pow(18));
 
             amount = new BN(250000000000); // 250 bil tokens
             contract_balance.toString().should.equal(amount.toString());
+            console.log('Initial Contract Balance: ' + contract_balance.toString());
         })
     })
 
-    describe("Child contract releases tokens properly..", async function(){
+    describe("releases tokens properly..", async function(){
         it('...fails to release, releaseTime not yet met.', async function() {
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             try {
-                await contract1.release();
+                await this.timeLock.release();
                 assert.fail("RelaseTime not met, should have failed.");
             } catch (err) {
                 assert.include(err.message, "revert", "The error message should contain 'revert'");
@@ -148,15 +78,12 @@ contract('ArtemisTimeLockFactory', accounts => {
         });
 
         it("...releases tranche 1", async function (){
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             const starting_balance = new BN(await this.token.balanceOf(accounts[3])).div(new BN(10).pow(18));
-            console.log("Starting Balance: " + starting_balance);
+            console.log("Initial Beneficiary Balance: " + starting_balance);
 
             timeJump(duration.weeks(24)); // jump ahead 24 weeks for the first tranche
 
-            await contract1.release();
+            await this.timeLock.release();
 
             const balance = new BN(await this.token.balanceOf(accounts[3])).div(new BN(10).pow(18));
 
@@ -165,11 +92,8 @@ contract('ArtemisTimeLockFactory', accounts => {
         })
 
         it('...fails to release tranche 2 early', async function() {
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             try {
-                await contract1.release();
+                await this.timeLock.release();
                 assert.fail("Early tranche release did not fail as asserted.");
             } catch (err) {
                 assert.include(err.message, "revert", "The error message should contain 'revert'");
@@ -177,12 +101,9 @@ contract('ArtemisTimeLockFactory', accounts => {
         });
 
         it("...releases tranche 2", async function (){
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             timeJump(duration.weeks(12)); // jump ahead 12 weeks to next tranche
 
-            await contract1.release();
+            await this.timeLock.release();
 
             const balance = new BN(await this.token.balanceOf(accounts[3])).div(new BN(10).pow(18));
 
@@ -191,11 +112,8 @@ contract('ArtemisTimeLockFactory', accounts => {
         })
 
         it('...fails to release tranche 3 early', async function() {
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             try {
-                await contract1.release();
+                await this.timeLock.release();
                 assert.fail("Early tranche release did not fail as asserted.");
             } catch (err) {
                 assert.include(err.message, "revert", "The error message should contain 'revert'");
@@ -203,12 +121,9 @@ contract('ArtemisTimeLockFactory', accounts => {
         });
 
         it("...releases tranche 3", async function (){
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             timeJump(duration.weeks(12)); // jump ahead 1 week into tranche 3
 
-            await contract1.release();
+            await this.timeLock.release();
 
             const balance = new BN(await this.token.balanceOf(accounts[3])).div(new BN(10).pow(18));
 
@@ -217,11 +132,8 @@ contract('ArtemisTimeLockFactory', accounts => {
         })
 
         it('...fails to release tranche 4 early', async function() {
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             try {
-                await contract1.release();
+                await this.timeLock.release();
                 assert.fail("Early tranche release did not fail as asserted.");
             } catch (err) {
                 assert.include(err.message, "revert", "The error message should contain 'revert'");
@@ -229,12 +141,9 @@ contract('ArtemisTimeLockFactory', accounts => {
         });
 
         it("...releases tranche 4", async function (){
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             timeJump(duration.weeks(12)); // jump ahead 12 weeks into tranche 4
 
-            await contract1.release();
+            await this.timeLock.release();
 
             const balance = new BN(await this.token.balanceOf(accounts[3])).div(new BN(10).pow(18));
 
@@ -243,11 +152,8 @@ contract('ArtemisTimeLockFactory', accounts => {
         })
 
         it('...fails to release tranche 5 early', async function() {
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             try {
-                await contract1.release();
+                await this.timeLock.release();
                 assert.fail("Early tranche release did not fail as asserted.");
             } catch (err) {
                 assert.include(err.message, "revert", "The error message should contain 'revert'");
@@ -255,12 +161,9 @@ contract('ArtemisTimeLockFactory', accounts => {
         });
 
         it("...releases tranche 5", async function (){
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             timeJump(duration.weeks(12));
 
-            await contract1.release();
+            await this.timeLock.release();
 
             const balance = new BN(await this.token.balanceOf(accounts[3])).div(new BN(10).pow(18));
 
@@ -268,11 +171,8 @@ contract('ArtemisTimeLockFactory', accounts => {
             balance.toString().should.equal('156250000000');
         })
         it('...fails to release tranche 6 early', async function() {
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             try {
-                await contract1.release();
+                await this.timeLock.release();
                 assert.fail("Early tranche release did not fail as asserted.");
             } catch (err) {
                 assert.include(err.message, "revert", "The error message should contain 'revert'");
@@ -280,12 +180,9 @@ contract('ArtemisTimeLockFactory', accounts => {
         });
 
         it("...releases tranche 6", async function (){
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             timeJump(duration.weeks(12));
 
-            await contract1.release();
+            await this.timeLock.release();
 
             const balance = new BN(await this.token.balanceOf(accounts[3])).div(new BN(10).pow(18));
 
@@ -294,11 +191,8 @@ contract('ArtemisTimeLockFactory', accounts => {
         })
 
         it('...fails to release tranche 7 early', async function() {
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             try {
-                await contract1.release();
+                await this.timeLock.release();
                 assert.fail("Early tranche release did not fail as asserted.");
             } catch (err) {
                 assert.include(err.message, "revert", "The error message should contain 'revert'");
@@ -306,12 +200,9 @@ contract('ArtemisTimeLockFactory', accounts => {
         });
 
         it("...releases tranche 7", async function (){
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             timeJump(duration.weeks(12));
 
-            await contract1.release();
+            await this.timeLock.release();
 
             const balance = new BN(await this.token.balanceOf(accounts[3])).div(new BN(10).pow(18));
 
@@ -320,11 +211,8 @@ contract('ArtemisTimeLockFactory', accounts => {
         })
 
         it('...fails to release tranche 8 early', async function() {
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             try {
-                await contract1.release();
+                await this.timeLock.release();
                 assert.fail("Early tranche release did not fail as asserted.");
             } catch (err) {
                 assert.include(err.message, "revert", "The error message should contain 'revert'");
@@ -332,12 +220,9 @@ contract('ArtemisTimeLockFactory', accounts => {
         });
 
         it("...releases tranche 8", async function (){
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             timeJump(duration.weeks(12));
 
-            await contract1.release();
+            await this.timeLock.release();
 
             const balance = new BN(await this.token.balanceOf(accounts[3])).div(new BN(10).pow(18));
 
@@ -346,11 +231,8 @@ contract('ArtemisTimeLockFactory', accounts => {
         })
 
         it('...fails release of additional tranche, zero tokens remaining.', async function() {
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
             try {
-                await contract1.release();
+                await this.timeLock.release();
                 assert.fail("Did not fail, additional tokens were released.");
             } catch (err) {
                 assert.include(err.message, "revert", "The error message should contain 'revert'");
@@ -358,7 +240,7 @@ contract('ArtemisTimeLockFactory', accounts => {
         });
 
         it('...contract received more tokens.', async function() {
-            const contract1_address = await this.contractFactory.timeLocks(0);
+            const contract1_address = await this.timeLock.address;
 
             amount = new BN(5000000000).times( new BN(10).pow(18));
             this.token.transfer(contract1_address, amount, {from: accounts[3]}); //  transfer tokens back into timelock
@@ -367,27 +249,8 @@ contract('ArtemisTimeLockFactory', accounts => {
             balance.toString().should.equal('5000000000');
         });
 
-        // Testing with timelock_v2 - does not require additional tranche to release in v2.
-        /*it("...fails to release tokens, requires 1 more tranche cycle.", async function (){
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
-            try {
-                await contract1.release();
-                assert.fail("Did not fail, additional tokens were released.");
-            } catch (err) {
-                assert.include(err.message, "revert", "The error message should contain 'revert'");
-            }
-        })*/
-
         it("...fully vested contract releases additional tokens.", async function (){
-            const contract1_address = await this.contractFactory.timeLocks(0);
-            contract1 = await ArtemisTimeLock.at(contract1_address);
-
-            // no time jump required in timelock_v2 - tokens should just release.
-            //timeJump(duration.weeks(12)); // jump ahead 12 weeks into tranche 5
-
-            await contract1.release();
+            await this.timeLock.release();
 
             const balance = new BN(await this.token.balanceOf(accounts[3])).div(new BN(10).pow(18));
 
